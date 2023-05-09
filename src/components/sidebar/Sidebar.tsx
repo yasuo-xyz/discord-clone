b80d3acb9a56b231f7934726de292e47be080da4
dora.xyz <dora.xyz@icloud.com>
2023-05-09 16:15:26 +0900

    型を定義

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -2,7 +2,19 @@ import React from 'react';
 import "./Sidebar.scss";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>
+  return (
+    // 型を定義
+    <div className="sidebar">
+        {/* サイドバーレフト（型） */}
+        {/* この型の中にはサーバーのアイコンなどを追加していく */}
+        <div className="sidebarLeft">
+
+        </div>
+        {/* サイドバーライト（型） */}
+        {/* この型の中にはチャンネルを追加していく */}
+        <div className="sidebarRight"></div>
+    </div>
+  )
 }
 
 export default Sidebar
