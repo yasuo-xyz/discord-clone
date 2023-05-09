5f531c5726fddcda86d759aa133912e77a2dc79d
dora.xyz <dora.xyz@icloud.com>
2023-05-09 16:05:52 +0900

    サイドーバーコンポーネントを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.scss';
+import Sidebar from './components/sidebar/Sidebar';
 
 function App() {
   return (
     <div className="App">
-      <h1>discord</h1>
+      {/* サイドバーコンポーネント */}
+      <Sidebar />
+
+      {/* ホーム */}
+
     </div>
   );
 }
