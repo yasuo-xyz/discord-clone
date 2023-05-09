28c115b93a7a8014a8b2264d3106314584967541
dora.xyz <dora.xyz@icloud.com>
2023-05-09 17:39:45 +0900

    HTMLを記述=サーバーアイコン、サイドバートップのヘッダー

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -9,10 +9,30 @@ const Sidebar = () => {
         {/* この型の中にはサーバーのアイコンなどを追加していく */}
         <div className="sidebarLeft">
 
+            {/* サーバーアイコン（型） */}
+            <div className="serverIcon">
+                {/* imgタグとは、画像を表示する為のタグ */}
+                <img src="./logo192.png" alt="" />
+            </div>
+
+            {/* サーバーアイコン（型） */}
+            <div className="serverIcon">
+                {/* imgタグとは、画像を表示する為のタグ */}
+                <img src="./logo192.png" alt="" />
+            </div>
+
         </div>
+
         {/* サイドバーライト（型） */}
         {/* この型の中にはチャンネルを追加していく */}
-        <div className="sidebarRight"></div>
+        <div className="sidebarRight">
+
+            {/* サイドバートップ（型） */}
+            <div className="sidebarTop">
+                <h3>Discord</h3>
+            </div>
+
+        </div>
     </div>
   )
 }
