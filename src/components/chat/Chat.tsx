49e392773f1118b1c2e9a5b3f2656b0aa4915f73
dora.xyz <dora.xyz@icloud.com>
2023-05-18 07:08:17 +0900

    チャットインプットのフッターを作成

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,16 +1,39 @@
 import React from 'react'
 import "./Chat.scss";
 import ChatHeader from './ChatHeader';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import CardGiftcardIcon from '@mui/icons-material/CardGiftcard';
+import GifIcon from '@mui/icons-material/Gif';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
 
 const Chat = () => {
   return (
     <div className="chat">
-    {/* チャットヘッダー */}
+    {/* チャットヘッダー（コンポーネント） */}
     <ChatHeader />
     {/* チャットメッセージ */}
     <div className="chatMessage"></div>
     {/* チャットインポート */}
-    <div className="chatInput"></div>
+    <div className="chatInput">
+      {/* アイコンをインポート */}
+      <AddCircleOutlineIcon />
+      {/* ホームタグを用意 */}
+      <form>
+        <input type="text" placeholder="#仮置きへのメッセージを送信" />
+        {/* チャットインプットボタン（型） */}
+        <button type="submit" className="chatInputButton">
+          送信
+        </button>
+      </form>
+
+      {/* チャットインプットアイコンズ（型） */}
+      {/* この中にはGIFとかを用意する */}
+      <div className="chatIputIcons">
+        <CardGiftcardIcon />
+        <GifIcon />
+        <EmojiEmotionsIcon />
+      </div>
+    </div>
     </div>
   )
 }
