9e9b71d305b514fe87eede054a57752d42c5be39
dora.xyz <dora.xyz@icloud.com>
2023-05-25 06:47:05 +0900

    ログインページを作成

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,35 @@ import React from 'react';
 import './App.scss';
 import Sidebar from './components/sidebar/Sidebar';
 import Chat from './components/chat/Chat';
+import Login from './components/login/Login';
 
 function App() {
+  // ログインページを記述
+  // const user = useSelector((state) => state.user.user);
+  // 仮置き記述
+  const user = null;
+
   return (
     <div className="App">
-      {/* サイドバーコンポーネント */}
-      <Sidebar />
+      {/* ユーザーがnullの時はログインページを見せるようにしていく */}
+      {/* 参考演算で記述していく */}
+      {/* ユーザー情報がある時の記述 */}
+      {user ? (
+        // trueの場合この中身を表示
+        <>
+          {/* サイドバーコンポーネント */}
+          <Sidebar />
 
-      {/* チャットコンポーネント */}
-      <Chat />
+          {/* チャットコンポーネント */}
+          <Chat />
+        </>
+      ) : (
+        // falseの場合この中身を表示（今の場合だとnullに設定されてるのでこちらが表示される）
+        <>
+          {/* ログインコンポーネント */}
+          <Login />
+        </>
+      )}
     </div>
   );
 }
