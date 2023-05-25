fec05ba0fef78a204f433c3297f307e5767dbe49
dora.xyz <dora.xyz@icloud.com>
2023-05-26 06:22:01 +0900

    ファイヤーベースのデータを取得

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import "./Sidebar.scss";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import AddIcon from '@mui/icons-material/Add';
@@ -6,11 +6,22 @@ import SidebarChannel from './SidebarChannel';
 import MicIcon from '@mui/icons-material/Mic';
 import HeadphonesIcon from '@mui/icons-material/Headphones';
 import SettingsIcon from '@mui/icons-material/Settings';
-import { auth } from '../../firebase';
+import { auth, db } from '../../firebase';
 import { useAppSelector } from '../../app/hooks';
+import { collection, query } from 'firebase/firestore/lite';
+import { onSnapshot } from "firebase/firestore";
 
 const Sidebar = () => {
     const user = useAppSelector((state) => state.user);
+
+    const q = query(collection(db, "channels"));
+
+    useEffect(() => {
+        onSnapshot(q, (querySnapshot) => {
+            const channelsResults = [];
+            querySnapshot.docs.forEach((doc) => console.log(doc));
+        });
+    }, []);
 
   return (
     // 型を定義
